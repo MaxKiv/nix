3f94dfef3ccbc1b6277cc78db77302042fb74eb1
MaxKiv <maxkivits42@gmail.com>
2024-03-20 17:34:29 +0100

    feat: wifi ssid/pass in sops

--- modules/hardware/network/default.nix
+++ modules/hardware/network/default.nix
@@ -1,4 +1,4 @@
-{ hostname, username, ... }:
+{ hostname, username, sops, ... }:
 
 {
   networking = {
@@ -11,7 +11,21 @@
     hostName = hostname;
   };
 
-
+  # Setup wifi SSID and psk
+  sops.secrets."wireless.env" = { };
+  # The entire wireless.env file containing all network SSID and PSK is a
+  # single secret, see:
+  # https://www.reddit.com/r/NixOS/comments/zneyil/using_sopsnix_to_amange_wireless_secrets/
+  networking.wireless.environmentFile = sops.secrets."wireless.env".path;
+  # Secrets (PSKs, passwords, etc.) can be provided without adding them to the
+  # world-readable Nix store by defining them in the environment file and
+  # referring to them in option networking.wireless.networks with the syntax
+  # @varname@. 
+  networking.wireless.networks = {
+    "@home_uuid@" = {
+      psk = "@home_psk@";
+    };
+  };
 
   users.users.${username} = {
     extraGroups = [ "networkmanager" ];
