4658ad800b4b73e8e54aa70170793f264f739700
MaxKiv <maxkivits42@gmail.com>
2024-09-12 05:38:02 +0200

    feat(embedded): added saleae logic software + udev rules

--- modules/dev/embedded/default.nix
+++ modules/dev/embedded/default.nix
@@ -4,6 +4,7 @@
     openocd # Open on chip debugger, device flashing
     qemu # emulate devices
     tio # serial console tty
+    saleae-logic-2 # Saleae logic analyzer software
   ];
 
   # plugdev: Allows members to mount (only with the options nodev and nosuid,
@@ -14,6 +15,9 @@
     extraGroups = [ "plugdev" "dialout" ];
   };
 
-  services.udev.packages = [ pkgs.openocd ];
+  # services.udev.packages: Installs packages that contain udev rules and 
+  # ensures that those rules are applied. It automatically installs the 
+  # necessary binaries and configurations required for udev.
+  services.udev.packages = [ pkgs.openocd pkgs.saleae-logic-2 ];
 
 }
