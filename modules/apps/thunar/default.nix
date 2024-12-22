e198a5021c5b95514ecf51c3ae6f7eeddaa5e141
MaxKiv <maxkivits42@gmail.com>
2024-12-22 06:48:49 +0100

    feat(thunar): update thunar with exiting plugins

--- modules/apps/thunar/default.nix
+++ modules/apps/thunar/default.nix
@@ -3,10 +3,20 @@
   username,
   ...
 }: {
+  # Thunar is a file explorer
   programs.thunar = {
     enable = true;
     plugins = with pkgs.xfce; [
       thunar-archive-plugin
+      thunar-volman
     ];
+  };
+
+  services = {
+    # Mount, trash, and other functionalities
+    gvfs.enable = true;
+
+    # Thumbnail support for images
+    tumbler.enable = true;
   };
 }
