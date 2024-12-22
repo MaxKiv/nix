7d9f7996cc238f9cd6c2a8299c96b8279080c924
MaxKiv <maxkivits42@gmail.com>
2024-12-22 06:48:12 +0100

    feat(spicetify): add spicetify, update justfile

--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
-{
-  home-manager,
-  inputs,
-  ...
+{ home-manager
+, inputs
+, ...
 }: {
   imports = [
     # HM import and settings
     home-manager.nixosModules.home-manager
     {
-      nixpkgs.overlays = [inputs.neovim-nightly-overlay.overlays.default];
+      nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
-      home-manager.sharedModules = [inputs.plasma-manager.homeManagerModules.plasma-manager];
+      home-manager.sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
       home-manager.backupFileExtension = "backup";
+      home-manager.extraSpecialArgs = { inherit inputs; };
     }
     ./assets
     ./hosts
