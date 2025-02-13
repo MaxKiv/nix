fc356876b1187e0acf3405ba3095fa3ca98e1a77
MaxKiv <maxkivits42@gmail.com>
2025-02-13 09:39:06 +0100

    feat(python): add python tooling, regression fix

--- modules/hardware/bluetooth/default.nix
+++ modules/hardware/bluetooth/default.nix
@@ -1,5 +1,20 @@
-{...}: {
+{username, ...}: {
   # Enable bluetooth support
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = false; # powers up the default Bluetooth controller on boot
+
+  # TODO: remove this when fix is upstreamed
+  # Fix Unit tray.target not found
+  home-manager.users.${username} = {
+    config,
+    pkgs,
+    ...
+  }: {
+    systemd.user.targets.tray = {
+      Unit = {
+        Description = "Home Manager System Tray";
+        Requires = ["graphical-session-pre.target"];
+      };
+    };
+  };
 }
