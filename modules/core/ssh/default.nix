13ff976db404ae552b125b7c42ed7af9d8d5b7b5
MaxKiv <maxkivits42@gmail.com>
2025-04-16 13:47:21 +0200

    feat(ssh): fix ssh configuration

--- modules/core/ssh/default.nix
+++ modules/core/ssh/default.nix
@@ -14,22 +14,52 @@
   sops.secrets = {
     "ssh/${hostname}" = {
       owner = "${username}";
-      path = "/home/${username}/.ssh/id_ed25519";
+      path = "/home/${username}/.ssh/${username}";
+    };
+    "ssh/saxion" = {
+      owner = "${username}";
+      path = "/home/${username}/.ssh/saxion";
     };
   };
 
-  home-manager.users.${username} = {
-    config,
-    pkgs,
-    ...
-  }: {
-    services.ssh-agent.enable = true;
-    programs.ssh.matchBlocks.${hostname}.identityFile = config.sops.secrets."ssh/${hostname}".path;
+  home-manager.users.${username} = let
+    saxionKeyPath = config.sops.secrets."ssh/saxion".path;
+    hostnameKeyPath = config.sops.secrets."ssh/${hostname}".path;
+  in
+    {
+      config,
+      pkgs,
+      ...
+    }: {
+      services.ssh-agent.enable = true;
+      programs.ssh = {
+        enable = true;
+        addKeysToAgent = "yes";
+        matchBlocks = {
+          github = {
+            host = "github.com";
+            user = "git";
+            identityFile = hostnameKeyPath;
+          };
+          gitlab = {
+            host = "gitlab.freedesktop.com";
+            user = "git";
+            identityFile = hostnameKeyPath;
+          };
+          bitbucket = {
+            host = "bitbucket.org";
+            user = "git";
+            identityFile = saxionKeyPath;
+            identitiesOnly = true;
+          };
+        };
+      };
 
-    home.file = {
-      # TODO make this reference a private repository, see:
-      # https://github.com/ryan4yin/nix-config/blob/985beb8bd47189e4b2ef5200ef5c1ab28e3812a8/home/base/desktop/ssh.nix#L4
-      ".ssh/id_ed25519.pub".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/git/nix/dotfiles/.ssh/${hostname}.pub";
+      home.file = {
+        # TODO make this reference a private repository, see:
+        # https://github.com/ryan4yin/nix-config/blob/985beb8bd47189e4b2ef5200ef5c1ab28e3812a8/home/base/desktop/ssh.nix#L4
+        ".ssh/id_ed25519.pub".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/git/nix/dotfiles/.ssh/${hostname}.pub";
+        ".ssh/saxion.pub".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/git/nix/dotfiles/.ssh/saxion.pub";
+      };
     };
-  };
 }
