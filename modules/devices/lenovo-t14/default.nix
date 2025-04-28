75c983adc2f9f412821acd44ec5ba74e4a3d49ea
MaxKiv <maxkivits42@gmail.com>
2025-04-28 09:50:24 +0200

    feat(gpg): improve gpg, add lenovo-t14

---
+++ modules/devices/lenovo-t14/default.nix
@@ -0,0 +1,23 @@
+{
+  hostname,
+  username,
+  lib,
+  config,
+  pkgs,
+  nixos-hardware,
+  ...
+}: {
+  # its a laptop!
+  imports = [
+    nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1
+    ../../hardware/laptop
+  ];
+
+  # Update amd microcode
+  hardware.cpu.amd.updateMicrocode = true;
+
+  # Use Lenovo firmware blobs
+  hardware.enableRedistributableFirmware = true;
+
+  my.grub-bootloader.enable = true;
+}
