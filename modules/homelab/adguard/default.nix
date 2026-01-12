e0dc6131273d476783b8ab50a73d2257929390ee
MaxKiv <maxkivits42@gmail.com>
2026-01-12 22:19:51 +0100

    feat(homelab): adguard -> pi-hole-esque dns sinkhole for ads and malware

---
+++ modules/homelab/adguard/default.nix
@@ -0,0 +1,57 @@
+{config, ...}: let
+  adguardPort = 3000;
+  fqdn = "ads.demtah.top";
+in {
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+    adguardPort
+  ];
+  networking.firewall.allowedUDPPorts = [53]; # DNS over UDP
+
+  services.adguardhome = {
+    enable = true;
+    host = "0.0.0.0";
+    settings = {
+      dns.upstream_dns = [
+        "1.1.1.1"
+        "9.9.9.9"
+      ];
+      filtering.protection_enabled = true;
+      filtering.filtering_enabled = true;
+      filters =
+        map (url: {
+          enabled = true;
+          url = url;
+        }) [
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt" # Steven black's hosts file
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_18.txt" # Filter Phishing domains
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_12.txt" # Anti-mailware list
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_30.txt" # Filter Phishing domains based on PhishTank and OpenPhish lists"
+        ];
+
+      theme = "dark";
+    };
+  };
+
+  services.nginx = {
+    virtualHosts = {
+      "${fqdn}" = {
+        default = false;
+        forceSSL = true;
+        useACMEHost = fqdn;
+
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString adguardPort}";
+        };
+      };
+    };
+  };
+
+  security.acme.certs."${fqdn}" = {
+    dnsProvider = "acme-dns";
+    environmentFile = config.sops.secrets.acme-dns-env.path;
+  };
+}
