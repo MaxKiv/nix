7ae46d281a67579ffbe3f808320776aec303c0cc
MaxKiv <maxkivits42@gmail.com>
2026-03-15 15:08:33 +0100

    feat(nfs): fix NFS client automount

--- modules/homelab/nfs/client.nix
+++ modules/homelab/nfs/client.nix
@@ -1,6 +1,11 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  username,
+  ...
+}: let
+  mountUser = username;
   server = "downtown.local";
-  dataDir = "/data";
+  dataDir = "/nas/data";
 in {
   # Install NFS utilities
   environment.systemPackages = with pkgs; [
@@ -8,19 +13,33 @@ in {
     rpcbind # RPC port mapper
   ];
 
-  fileSystems."/mnt/nas" = {
-    device = "${server}:${dataDir}";
-    fsType = "nfs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=600"
-      "rw"
-      "noatime"
-      "nodiratime"
-      "nfsvers=4"
-    ]; # lazy mount + disconnects after 10 minutes (i.e. 600 seconds)
-  };
+  # Working manual command:
+  # mount -t cifs //192.168.1.2/data /mnt/nas -o vers=3.1.1,guest,uid=$(id -u max),gid=$(id -g max),file_mode=0775,dir_mode=0775
+
+  # make sure the mount dir exists
+  systemd.tmpfiles.rules = [
+    "d /mnt/nas 0755 root root -"
+  ];
+
+  systemd.mounts = [
+    {
+      what = "${server}:${dataDir}";
+      where = "/mnt/nas";
+      type = "nfs";
+      options = "noatime,nfsvers=4";
+    }
+  ];
+
+  systemd.automounts = [
+    {
+      wantedBy = ["multi-user.target"];
+      automountConfig = {
+        TimeoutIdleSec = "600";
+      };
+      where = "/mnt/nas";
+    }
+  ];
+
   # optional, but ensures rpc-statsd is running for on demand mounting
   boot.supportedFilesystems = ["nfs"];
 }
